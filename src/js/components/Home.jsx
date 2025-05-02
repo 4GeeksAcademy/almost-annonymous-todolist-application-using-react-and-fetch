b6405511a34b444bb0294ef06bf045ad2dd8ee67
almost-annonymous <anam.dhala@gmail.com>
2025-05-02 23:17:19 +0000

    added a button to delete a task

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,35 +5,46 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [ inputValue, setInputValue ] = useState("")
-	const [ todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-	return (
-		<div className="container">
-			<h1>My Todos</h1>
-			<ul>
-				<li>
-					<input 
-						type="text" 
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyDown={(e) => {
-							if (e.key === "Enter") {
-								setTodos(todos.concat(inputValue)); 
-								setInputValue("");
-							}
-						}}
-						placeholder="What do you need to do?"></input>
-					</li>
-					{todos.map((t) => (
-						<li>
-						{t}
-						</li>
-					))}
-			</ul>
-			<div> {todos.length} tasks</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>My To Do List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+            placeholder="Tasks to do today"
+          ></input>
+        </li>
+        {todos.map((item, index) => (
+          <li>
+            <div className="d-flex justify-content-between">
+              {item}{" "}
+              <i
+                class="fa-solid fa-trash"
+                onClick={() =>
+                  setTodos(
+                    todos.filter((t, currentIndex) => index != currentIndex)
+                  )
+                }
+              ></i>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div> {todos.length} tasks</div>
+    </div>
+  );
 };
 
 export default Home;
